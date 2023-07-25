d6c71e1c0a7218c9a2e7453fa169254b46ba7fe7
bartekar <55534106+bartekar@users.noreply.github.com>
2023-07-25 21:27:30 +0200

    [ADD] people detection

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,25 @@
 using namespace cv;
 using namespace std;
 
+/*
+
+sources:
+- object detection: code + neural network
+https://github.com/doleron/yolov5-opencv-cpp-python/blob/main/cpp/yolo.cpp
+- object tracking:
+TBD
+
+*/
+
+struct MyBBox
+{
+  int class_id;
+  float confidence;
+  Rect bbox;
+};
+
+typedef struct MyBBox MyBBox;
+
 void draw_bbox(Mat img, Rect r)
 {
   if (r.x + r.width > img.cols)
@@ -33,6 +52,82 @@ void draw_bbox(Mat img, Rect r)
   }
 }
 
+void detect_humans(Mat img, vector<struct MyBBox> &output)
+{
+  const float INPUT_WIDTH = 640.0;
+  const float INPUT_HEIGHT = 640.0;
+  const float SCORE_THRESHOLD = 0.2;
+  const float NMS_THRESHOLD = 0.4;
+  const float CONFIDENCE_THRESHOLD = 0.4;
+
+  dnn::Net nnet = dnn::readNet("../yolov5s.onnx"); // todo: remove unnecessary files out of the repository
+  nnet.setPreferableBackend(dnn::DNN_BACKEND_OPENCV);
+  nnet.setPreferableTarget(dnn::DNN_TARGET_CPU);
+
+  int mx = MAX(img.cols, img.rows);
+  Mat img_square = Mat::zeros(mx, mx, CV_8UC3);
+  img.copyTo(img_square(Rect(0, 0, img.cols, img.rows)));
+
+  Mat input_img;
+  dnn::blobFromImage(img_square, input_img, 1./255., Size(INPUT_WIDTH, INPUT_HEIGHT), cv::Scalar(), true, false); // preprocessing
+  nnet.setInput(input_img);
+
+  vector<Mat> outputs;
+  nnet.forward(outputs, nnet.getUnconnectedOutLayersNames());
+
+  float x_factor = img_square.cols / INPUT_WIDTH;
+  float y_factor = img_square.rows / INPUT_HEIGHT;
+
+  float *data = (float *)outputs[0].data;
+
+  const int dimensions = 85;
+  const int rows = 25200;
+
+  std::vector<int> class_ids;
+  std::vector<float> confidences;
+  std::vector<cv::Rect> boxes;
+
+  for (int i = 0; i < rows; ++i)
+  {
+    float confidence = data[4];
+    if (confidence >= CONFIDENCE_THRESHOLD)
+    {
+      float * classes_scores = data + 5;
+      Mat scores(1, 80, CV_32FC1, classes_scores);
+      Point class_id;
+      double max_class_score;
+      minMaxLoc(scores, 0, &max_class_score, 0, &class_id);
+      if (max_class_score > SCORE_THRESHOLD)
+      {
+        confidences.push_back(confidence);
+        class_ids.push_back(class_id.x);
+        float x = data[0];
+        float y = data[1];
+        float w = data[2];
+        float h = data[3];
+        int left = int((x - 0.5 * w) * x_factor);
+        int top = int((y - 0.5 * h) * y_factor);
+        int width = int(w * x_factor);
+        int height = int(h * y_factor);
+        boxes.push_back(Rect(left, top, width, height));
+      }
+    }
+    data += 85;
+  }
+
+  std::vector<int> nms_result;
+  cv::dnn::NMSBoxes(boxes, confidences, SCORE_THRESHOLD, NMS_THRESHOLD, nms_result);
+  for (int i = 0; i < nms_result.size(); i++)
+  {
+    int idx = nms_result[i];
+    MyBBox result;
+    result.class_id = class_ids[idx];
+    result.confidence = confidences[idx];
+    result.bbox = boxes[idx];
+    output.push_back(result);
+  }
+}
+
 int main(int argc, char** argv)
 {
   if ( argc != 2 )
@@ -56,12 +151,24 @@ int main(int argc, char** argv)
   cvtColor( input_img, img, COLOR_RGB2BGR ); // rgb -> bgr, so that one can actually see the results
   // from now on it is bgr
 
+//  dnn::Net net = dnn::readNet("../yolov5s.onnx"); // todo: remove unnecessary files out of the repository
+//  net.setPreferableBackend(dnn::DNN_BACKEND_OPENCV);
+//  net.setPreferableTarget(dnn::DNN_TARGET_CPU);
+
+  vector<MyBBox> people;
+  detect_humans(img, people);
+  for (vector<MyBBox>::iterator iter = people.begin(); iter != people.end(); ++iter)
+  {
+    draw_bbox(input_img, iter->bbox);
+  }
+
+
   vector<int> compression_params;
   compression_params.push_back(IMWRITE_PNG_COMPRESSION);
   compression_params.push_back(9);
   try
   {
-    imwrite("modded.png", img, compression_params);
+    imwrite("modded.png", input_img, compression_params);
   }
   catch (const cv::Exception& ex)
   {
@@ -69,7 +176,7 @@ int main(int argc, char** argv)
   }
 
   namedWindow("Display Image", WINDOW_AUTOSIZE );
-  imshow("Display Image", img);
+  imshow("Display Image", input_img);
   waitKey(0);
   return 0;
 }
